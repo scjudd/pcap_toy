46065041d71773eefc49c1e7e381b147a15b4087
Spencer Judd <spencercjudd@gmail.com>
2014-09-02 21:40:22 -0400

    Refactor.

--- pcap_test.c
+++ pcap_test.c
@@ -5,8 +5,8 @@
 
 #define ETHER_ADDR_FMT_LEN  18
 
-int format_ether_addr(const u_int8_t *adr, char *dst) {
-    return sprintf(dst, "%02x:%02x:%02x:%02x:%02x:%02x",
+void format_ether_addr(const u_int8_t *adr, char *dst) {
+    sprintf(dst, "%02x:%02x:%02x:%02x:%02x:%02x",
             adr[0], adr[1], adr[2], adr[3], adr[4], adr[5]);
 }
 
@@ -17,10 +17,10 @@ void got_packet(u_char *user, const struct pcap_pkthdr *h, const u_char *packet)
     const struct ether_header *ethernet;
     ethernet = (struct ether_header*)packet;
 
-    char src_addr[ETHER_ADDR_FMT_LEN]; // Source Ethernet host
+    char src_addr[ETHER_ADDR_FMT_LEN];
     format_ether_addr(ethernet->ether_shost, src_addr);
 
-    char dst_addr[ETHER_ADDR_FMT_LEN]; // Destination Ethernet host
+    char dst_addr[ETHER_ADDR_FMT_LEN];
     format_ether_addr(ethernet->ether_dhost, dst_addr);
 
     printf("Packet #%d:\n", count);
@@ -29,58 +29,60 @@ void got_packet(u_char *user, const struct pcap_pkthdr *h, const u_char *packet)
 }
 
 int main(int argc, char *argv[]) {
+
+    if (argc < 2 || argc > 3) {
+        fprintf(stderr, "usage: %s <device> [filter]\n", argv[0]);
+        exit(1);
+    }
+
+    const char *device = argv[1];
+    const char *filter = (argc > 2) ? argv[2] : "";
+
+    // used to store an error message before a pcap_t is created
     char errbuf[PCAP_ERRBUF_SIZE];
 
-    bpf_u_int32 net;        // Our IP
-    bpf_u_int32 mask;       // Our netmask
-    struct bpf_program fp;  // The compiled filter
-
-    const char *filter = "ip and port 80";
-    const char *device = argv[1];
-
-    if (argc == 1) {
-        fprintf(stderr, "usage: %s <device>\n", argv[0]);
+    bpf_u_int32 ip;
+    bpf_u_int32 netmask;
+    if (pcap_lookupnet(device, &ip, &netmask, errbuf) != 0) {
+        fprintf(stderr, "pcap_lookupnet: %s\n", errbuf);
         exit(1);
     }
 
-    // Find properties from capture device
-    if (pcap_lookupnet(device, &net, &mask, errbuf) != 0) {
-        fprintf(stderr, "pcap_lookupnet error: %s\n", errbuf);
-        exit(1);
-    }
-
-    // Capture from ethernet device
     pcap_t *handle = pcap_create(device, errbuf);
     if (handle == NULL) {
-        fprintf(stderr, "pcap_create error: %s\n", errbuf);
+        fprintf(stderr, "pcap_create: %s\n", errbuf);
         exit(1);
     }
 
-    // Activate the capture handle
+    if (pcap_set_timeout(handle, 1000) != 0) {
+        pcap_perror(handle, "pcap_set_timeout");
+        exit(1);
+    }
+
     if (pcap_activate(handle) != 0) {
         pcap_perror(handle, "pcap_activate");
         exit(1);
     }
 
-    // Compile capture filter
-    if (pcap_compile(handle, &fp, filter, 0, net) != 0) {
+    struct bpf_program fp;  // the compiled filter
+    if (pcap_compile(handle, &fp, filter, 0, ip) != 0) {
         pcap_perror(handle, "pcap_compile");
         exit(1);
     }
 
-    // Set capture filter
     if (pcap_setfilter(handle, &fp) != 0) {
         pcap_perror(handle, "pcap_setfilter");
         exit(1);
     }
 
-    printf("Starting '%s' capture.\n", filter);
+    if (argc > 2) {
+        printf("Starting \"%s\" capture on %s.\n", filter, device);
+    } else {
+        printf("Starting capture on %s.\n", device);
+    }
 
-    // Enter pcap_loop
     if (pcap_loop(handle, -1, got_packet, NULL) != 0) {
         pcap_perror(handle, "pcap_loop");
         exit(1);
     }
-
-    return 0;
 }
